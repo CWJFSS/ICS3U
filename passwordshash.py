35ea2c93ba47cd442d394ae10f3047ddd50a70a9
CWJFSS <cw3141now@gmail.com>
2018-06-04 09:17:20 -0400

    Update passwordshash.py

--- passwordshash.py
+++ passwordshash.py
@@ -1,7 +1,11 @@
+#Made by: CW
+#date: 5/27/17
+#teacher: Mr. Seidel
+#uses hash function with 3 users to show assignments A and B
+
+
 import uuid
 import hashlib
-
-
 
 
 with open('database.txt', 'r') as f:
@@ -16,73 +20,49 @@ with open('database.txt', 'r') as f:
     
     codify = uuid.uuid4().hex
 
-    hashedA=(hashlib.sha256(codify.encode() + lines[0].encode()).hexdigest() + ':' + codify)
-    hashedB=(hashlib.sha256(codify.encode() + lines[1].encode()).hexdigest() + ':' + codify)
-    hashedAB=(hashlib.sha256(codify.encode() + lines[2].encode()).hexdigest() + ':' + codify)
+    hashedA=(hashlib.sha224(codify.encode() + lines[0].encode()).hexdigest() + ':' + codify)
+    hashedB=(hashlib.sha224(codify.encode() + lines[1].encode()).hexdigest() + ':' + codify)
+    hashedAB=(hashlib.sha224(codify.encode() + lines[2].encode()).hexdigest() + ':' + codify)
 
-    input_pass=(hashlib.sha256(codify.encode() + input('Please enter a password: ').encode()).hexdigest() + ':' + codify)
+    input_pass=(hashlib.sha224(codify.encode() + input('Please enter a password: ').encode()).hexdigest() + ':' + codify)
     
     
     if input_pass==hashedA:
-        print("good")
+	
+        import house
+		import arcs
+		import partAnum3
+		import exc4partA
+		import exc5partA
+		import circlefollowmouse
+		import assignmentApartLast
+		
     if input_pass==hashedB:
-        print("leboi")
+	
+		import partBexcNine
+		import partBexcTen
+		import partBexcEleven
+		import partBexcTwelve
+		
     if input_pass==hashedAB:
-        print("dasmaboi")
+        import house
+		import arcs
+		import partAnum3
+		import exc4partA
+		import exc5partA
+		import circlefollowmouse
+		import assignmentApartLast
+		import partBexcNine
+		import partBexcTen
+		import partBexcEleven
+		import partBexcTwelve
     else:
-        print("bad")
+        print("Your password does not exist. Please try again ")
 
-        
-    """new_pass = input('Please enter a password: ')
-    if new_pass==lines[0]:
-        print("good")
-    else:
-        print("bad")"""
-
-
-
-"""def hash_password(password):
-    # uuid is used to generate a random number
-    codify = uuid.uuid4().hex
-    return hashlib.sha256(codify.encode() + password.encode()).hexdigest() + ':' + codify
-
-def check_password(hashed_password, user_password):
-    password, codify = hashed_password.split(':')
-    return password == hashlib.sha256(codify.encode() + user_password.encode()).hexdigest()"""
-"""def hash_password(password):
-    # uuid is used to generate a random number
-    codify = uuid.uuid4().hex
-    return hashlib.sha256(salt.encode() + password.encode()).hexdigest() + ':' + codify
     
-def check_password(hashed_password, user_password):
-    password, salt = hashed_password.split(':')
-    return password == hashlib.sha256(salt.encode() + user_password.encode()).hexdigest()
 
-new_pass = input('Please enter a password: ')
-hashed_password = hash_password(new_pass)
-print('The string to store in the db is: ' + hashed_password)
-old_pass = input('Now please enter the password again to check: ')
-#if check_password(hashed_password, old_pass):
-#    import house
-#    import arcs
-#    import partAnum3
-#    import exc4partA
-#    import exc5partA
-#    import circlefollowmouse
-#    import assignmentApartLast
-    
-if check_password(hashed_password, old_pass):
-    #import partBexcNine
-    #import partBexcTen
-    #import partBexcEleven
-    #import partBexcTwelve
-
-if check_password(hashed_password, old_pass):
-import all
 
 
 
 
     
-else:
-    print('I am sorry but the password does not match')"""
