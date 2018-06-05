508f86fddd7d5911fb00fc5e8b3d437e8fb32d45
CWJFSS <cw3141now@gmail.com>
2018-06-05 12:18:23 -0400

    Update exc5partA.py

--- exc5partA.py
+++ exc5partA.py
@@ -1,9 +1,6 @@
-# Import a library of functions called 'pygame'
 import pygame
 from math import pi
 import random
-# Initialize the game engine
-#NOTE: xpos, ypos is the x position and y position respectively of the small circles
 pygame.init()
 import random
 
@@ -15,10 +12,7 @@ COLORS = [(0, 0, 0),
           ,(200,200,0),
          ( 200,0,0)]
 
-"""def random_color():
-    return random.choice(COLORS)
- """
-# Define the colors we will use in RGB format
+
 
 WHITE = (255, 255, 255)
 BLUE =  (  0,   0, 255)
@@ -30,27 +24,22 @@ largeCircleXPos=200
 largeCircleYPos= 225
 bigRadius=70
 
-# Set the height and width of the screen
 size = [400, 300]
 screen = pygame.display.set_mode(size)
 
 pygame.display.set_caption("Example code for the draw module")
- 
-#Loop until the user clicks the close button.
+
 done = False
 clock = pygame.time.Clock()
 
-#rand_color=random(1,30)
 
 while not done:
- 
-    # This limits the while loop to a max of 10 times per second.
-    # Leave this out and we will use all CPU we can.
+
     clock.tick(10)
      
-    for event in pygame.event.get(): # User did something
-        if event.type == pygame.QUIT: # If user clicked close
-            done=True # Flag that we are done so we exit this loop
+    for event in pygame.event.get(): 
+        if event.type == pygame.QUIT: # 
+            done=True # 
 
 
     rad=20
@@ -59,10 +48,7 @@ while not done:
         for ypos in range(0,300,20):
             pygame.draw.circle(screen, (ypos+100), [xpos+int(ypos/6), ypos], 10)
 
-    """color*=20"""
-    # Go ahead and update the screen with what we've drawn.
-    # This MUST happen after all the other drawing commands.
+   
     pygame.display.flip()
 
-# Be IDLE friendly
 pygame.quit()
