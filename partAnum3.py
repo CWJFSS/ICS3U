5885fed2023422b441ab18f3f22d662249155917
CWJFSS <cw3141now@gmail.com>
2018-06-05 12:08:38 -0400

    Update partAnum3.py

--- partAnum3.py
+++ partAnum3.py
@@ -1,11 +1,8 @@
-# Import a library of functions called 'pygame'
 import pygame
 from math import pi
 import random
-# Initialize the game engine
 pygame.init()
 import random
-#col = random.randint(0, 200)
 COLORS = [(0, 0, 0), 
           (0, 200, 0),
           (0, 0, 200),
@@ -14,21 +11,16 @@ COLORS = [(0, 0, 0),
           ,(200,200,0),
          ( 200,0,0)]
 
-"""def random_color():
-    return random.choice(COLORS)
- """
-# Define the colors we will use in RGB format
+
 
 WHITE = (255, 255, 255)
 
 
-# Set the height and width of the screen
 size = [400, 300]
 screen = pygame.display.set_mode(size)
 
 pygame.display.set_caption("Example code for the draw module")
 screen.fill(WHITE)
-#Loop until the user clicks the close button.
 done = False
 clock = pygame.time.Clock()
  
@@ -36,9 +28,9 @@ while not done:
 
     clock.tick(30)
      
-    for event in pygame.event.get(): # User did something
-        if event.type == pygame.QUIT: # If user clicked close
-            done=True # Flag that we are done so we exit this loop
+    for event in pygame.event.get(): 
+        if event.type == pygame.QUIT: 
+            done=True 
 
         
     x, y = pygame.mouse.get_pos()    
@@ -48,16 +40,10 @@ while not done:
 
     bgr = (random.randint(0,255), random.randint(0,255), random.randint(0,255))   
     circe=pygame.draw.circle(screen,(y), [x, y], 25)
-    #circe.fill(bgr)
-
-    #if pygame.time.get_ticks()-timer>1000:
-    #    timer=pg.time.get_ticks()
-    #    bgr=(random.randint(0,255), random.randint(0,255),random.randint(0,255))
 
 
-    #circe=pygame.draw.circle(screen,(COLR), [x, y], 25)
+
 
     pygame.display.flip()
 
-# Be IDLE friendly
 pygame.quit()
