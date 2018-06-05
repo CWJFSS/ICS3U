7e3ec950086247caa842e9d6525e43cd077ac88b
CWJFSS <cw3141now@gmail.com>
2018-06-05 10:57:31 -0400

    Update partBexcNine.py

--- partBexcNine.py
+++ partBexcNine.py
@@ -23,12 +23,10 @@ import random
 import pygame
 from math import pi
 
-# Initialize the game engine
-#point=(numpy.random.randint(125, size=50))
+
 pygame.init()
 
 
-# Define the colors we will use in RGB format
 
 WHITE = (255, 255, 255)
 BLUE =  (  0,   0, 255)
@@ -36,31 +34,28 @@ GREEN = (  0, 255,   0)
 RED =   (255,   0,   0)
 
 
-# Set the height and width of the screen
 size = [600,  600]
 screen = pygame.display.set_mode(size)
 
 pygame.display.set_caption("Example code for the draw module")
  
-#Loop until the user clicks the close button.
 done = False
 clock = pygame.time.Clock()
 screen.fill((WHITE))
-#rand_color=random(1,30)
+
 
 topWall=250
 bottomWall=0
 xpos=0
 while not done:
  
-    # This limits the while loop to a max of 10 times per second.
-    # Leave this out and we will use all CPU we can.
+
     clock.tick(10)
     
 
-    for event in pygame.event.get(): # User did something
-        if event.type == pygame.QUIT: # If user clicked close
-            done=True # Flag that we are done so we exit this loop
+    for event in pygame.event.get(): 
+        if event.type == pygame.QUIT:
+            done=True 
 
         for i in range(0,50,1):
             
@@ -73,26 +68,10 @@ while not done:
             if ypos[i]<300:
                 strt_x_pos=0
             pygame.draw.lines(screen, RED, True, [(xpos,strt_x_pos), (xpos,ypos[i])], 5)
-            #for point in range (0,1,1):
-            
-            
-            #point=(numpy.random.randint(600, size=2))
-            #pygame.draw.circle(screen, RED,(point),11 ,11)
-            #xpos=10
-            #pygame.draw.lines(screen, RED, True, [(xpos,0), (xpos,point)], 10)
 
-
-    #for xpos_down in range(0,250,20):
-    #    point=(numpy.random.randint(125, size=50)) 
-    #    pygame.draw.lines(screen, RED, True, [(xpos_down,250), (xpos_down,240)], 10)
-
-    #if clock.tick==5000:
-    #    pygame.display.quit()
-    # Go ahead and update the screen with what we've drawn.
-    # This MUST happen after all the other drawing commands.
     pygame.display.flip()
 
-# Be IDLE friendly
+
 pygame.quit()
 
 
