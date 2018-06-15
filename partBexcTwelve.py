d50abe487fbc779c0e5c65ddbce9c886b73dbb17
CWJFSS <cw3141now@gmail.com>
2018-06-15 12:35:28 -0400

    Update partBexcTwelve.py

--- partBexcTwelve.py
+++ partBexcTwelve.py
@@ -8,11 +8,11 @@ Use macbeth.txt for finding out the most used letter in the file"""
 #Made by: Clark W
 #date: 5/27/17
 #teacher: Mr. Seidel
-
+#get libraries
 import os
 import collections
 
-
+#read all the files and set as variables
 with open("illiad.txt",'r') as a:
     message1=str(a.read())    
 with open("othello.txt",'r') as b:
@@ -29,23 +29,25 @@ with open("macbeth.txt",'r') as e:
 
 with open("hamlet.txt",'r') as f:
     message=str(f.read())
-    
+#counts words    
 words=message4.split()
 wordCount=len(words)
+#counts characters
 charCount=len(message2)
-    
+#prints it    
 print(wordCount)
 print(charCount)
-
+#gets capitals
 messageHigh=sum(1 for c in message1 if c.isupper())
 print("the amount of capitals is: "+str(messageHigh))
-amt = lambda l1, l2: len(list(filter(lambda c: c in l2, l1)))
 
+amt = lambda l1, l2: len(list(filter(lambda c: c in l2, l1)))
+#gets all the punctuations
 punct = amt(message, string.punctuation)
 
 
 amt = lambda l1, l2: len(list(filter(lambda c: c in l2, l1)))
 print("the most common charcter ____occurs____times "+str(collections.Counter(message5).most_common(1)[0]))
 print("the vowels aeiou respectively are:")
-
+#gets all the vowels
 print(*map(message3.lower().amt, "aeiou"))
