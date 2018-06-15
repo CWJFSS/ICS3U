c687df9b97d84dc903f1f972b2a393b47a1da472
CWJFSS <cw3141now@gmail.com>
2018-06-15 12:43:41 -0400

    Update assignmentAexc7.py

--- assignmentAexc7.py
+++ assignmentAexc7.py
@@ -2,7 +2,7 @@
 #date: 5/27/17
 #teacher: Mr. Seidel
 
-
+#get libs
 import pygame
 from math import pi
 import random
@@ -13,13 +13,13 @@ import random
 
 
 
-
+#define all cols
 WHITE = (255, 255, 255)
 BLUE =  (  0,   0, 255)
 GREEN = (  0, 255,   0)
 RED =   (255,   0,   0)
 
-
+#set variables
 largeCircleXPos=200
 largeCircleYPos= 225
 bigRadius=70
@@ -33,7 +33,7 @@ clock = pygame.time.Clock()
 
 
 
- 
+ #game loop
 while not done:
  
 
@@ -43,14 +43,14 @@ while not done:
         if event.type == pygame.QUIT: 
             done=True
  
-
+#for loop for the colours and the big circle
     for redBig in range(1,80,2):
         for  greenBig in range(3,250,5):
             for blueBig in range(30, 200,20):
                 for bigRadius in range (1,70,2):
                     pygame.draw.circle(screen, (redBig,greenBig,blueBig), [largeCircleXPos, largeCircleYPos], bigRadius)
  
-
+#smaller circles, for loop to draw them in a box
     smallColor=0
     for xpos in range(0,450,20):
             for ypos in range(0,150,20):
