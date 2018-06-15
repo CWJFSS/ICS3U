ff62879ac906aa1dbdef266f516e9ec6a1de11fd
CWJFSS <cw3141now@gmail.com>
2018-06-15 12:45:14 -0400

    Update partAexc3.py

--- partAexc3.py
+++ partAexc3.py
@@ -2,7 +2,7 @@
 #date: 5/27/17
 #teacher: Mr. Seidel
 
-
+#imports everything needed
 import pygame
 from math import pi
 import random
@@ -18,31 +18,32 @@ WHITE = (255, 255, 255)
 size = [400, 300]
 screen = pygame.display.set_mode(size)
 
-pygame.display.set_caption("Example code for the draw module")
+pygame.display.set_caption("homework")
 screen.fill(WHITE)
+#allows the program to not close
 done = False
 clock = pygame.time.Clock()
  
 while not done:
-
+#30 fps clock means runs 30 times  per second
     clock.tick(30)
      
     for event in pygame.event.get(): 
         if event.type == pygame.QUIT: 
             done=True 
 
-        
+        #gets mouse position
     x, y = pygame.mouse.get_pos()    
 
 
-
+#draws a circle continously at the mouse position as the center
 
     bgr = (random.randint(0,255), random.randint(0,255), random.randint(0,255))   
     circe=pygame.draw.circle(screen,(y), [x, y], 25)
 
 
 
-
+#shows the screen
     pygame.display.flip()
 
 pygame.quit()
