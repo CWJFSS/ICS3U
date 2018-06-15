b591e3d729d4e05c1ea997b7ccac29286c227613
CWJFSS <cw3141now@gmail.com>
2018-06-15 12:48:33 -0400

    Update partAexc1.py

--- partAexc1.py
+++ partAexc1.py
@@ -4,9 +4,9 @@
 import pygame
 pygame.init()
 screen = pygame.display.set_mode((640,480))
+#mode is screensize()
 
-
-
+#all the functions are same in processing, you just need to say you draw to the screen
 pygame.draw.rect(screen,(0,0,255),(0,0,640,400))
 pygame.draw.circle(screen,(255,255,0),(50,400),200)
 
@@ -23,10 +23,11 @@ pygame.draw.polygon(screen, (0,0,0), ( (255,400-180),(255+100,400-250),(255+200,
 
 
 pygame.display.flip()
-
+#the loop that keeps it all running
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+#out of the loop a as this is when running is not true
 pygame.quit()
